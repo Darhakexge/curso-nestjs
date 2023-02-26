335d2264b62d605fc1c177b1a417bfd6719e30af
Juan David Moyano Gutiérrez <darhakexge@gmail.com>
2023-02-26 16:58:14 -0500

    Implementada la creación de Pokémon

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,11 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import {
+    BadRequestException,
+    Injectable,
+    InternalServerErrorException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { Pokemon } from './entities/pokemon.entity';
 
 @Injectable()
 export class PokemonService {
-    create(createPokemonDto: CreatePokemonDto) {
-        return 'This action adds a new pokemon';
+    private readonly pokemonModel: Model<Pokemon>;
+
+    constructor(@InjectModel(Pokemon.name) pokemonModel: Model<Pokemon>) {
+        this.pokemonModel = pokemonModel;
+    }
+
+    async create(createPokemonDto: CreatePokemonDto) /* : Promise<Pokemon> */ {
+        try {
+            const pokemon = await this.pokemonModel.create(createPokemonDto);
+
+            return pokemon;
+        } catch (error) {
+            if (error.code === 11000) {
+                const [key, value] = Object.entries(error.keyValue)[0];
+                throw new BadRequestException(
+                    `The Pokémon with ${value} ${key} already exists`,
+                );
+            }
+
+            throw new InternalServerErrorException('Server error');
+        }
     }
 
     findAll() {
